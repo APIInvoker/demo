89c6131b86cea75118b41beca39db784501df29b
APIInvoker <zx820@outlook.com>
2022-08-09 21:01:15 +0800

    fastjson2

--- spring-unity/src/main/java/com/example/springunity/controller/UnityController.java
+++ spring-unity/src/main/java/com/example/springunity/controller/UnityController.java
@@ -1,6 +1,6 @@
 package com.example.springunity.controller;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.example.springunity.pojo.PayOrderVo;
 import com.example.springunity.pojo.ProductInfo;
 import com.example.springunity.service.impl.ProductInfoServiceImpl;
@@ -100,7 +100,7 @@ public class UnityController {
         log.info("入参:" + JSON.toJSONString(reqMap));
 
         log.info("组建签名参数map");
-        Map<String, Object> paramMap = new HashMap<String, Object>();
+        Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("payKey", payKey);
         paramMap.put("productName", productName);
         paramMap.put("orderNo", orderNo);
