beda31805d9f4aa80cfce92f26ea2cc69722244c
APIInvoker <zx820@outlook.com>
2022-09-04 15:59:15 +0800

    条件查询

--- spring-unity/src/main/java/com/example/springunity/mapper/UserInfoMapper.java
+++ spring-unity/src/main/java/com/example/springunity/mapper/UserInfoMapper.java
@@ -6,16 +6,25 @@ import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 
 /**
- * @author zx
- * @description 针对表【dev_user_info(用户信息表)】的数据库操作Mapper
- * @createDate 2022-08-16 23:03:28
- * @Entity com.example.springunity.entity.UserInfo
- */
+* @author zx
+* @description 针对表【dev_user_info(用户信息表)】的数据库操作Mapper
+* @createDate 2022-09-04 14:21:31
+* @Entity com.example.springunity.pojo.UserInfoDO
+*/
 @Mapper
 public interface UserInfoMapper {
-    List<UserInfoDO> selectAllUserInfo();
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UserInfoDO record);
+
+    int insertSelective(UserInfoDO record);
+
+    UserInfoDO selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(UserInfoDO record);
+
+    int updateByPrimaryKey(UserInfoDO record);
+
+    List<UserInfoDO> selectByCondition(UserInfoDO userInfoDO);
 }
-
-
-
-
---
+++ spring-unity/src/main/java/com/example/springunity/pojo/dto/UserInfoDTO.java
@@ -0,0 +1,48 @@
+package com.example.springunity.pojo.dto;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 用户信息表
+ *
+ * @TableName dev_user_info
+ */
+@Data
+public class UserInfoDTO implements Serializable {
+    /**
+     * 用户id
+     */
+    private Long userId;
+
+    /**
+     * 昵称
+     */
+    private String nickName;
+
+    /**
+     * 性别：1-男，2-女，3-未知
+     */
+    private Integer sex;
+
+    /**
+     * 用户年龄
+     */
+    private Integer age;
+
+    /**
+     * 生日
+     */
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date birthday;
+
+    /**
+     * 收入
+     */
+    private Long income;
+
+    private static final long serialVersionUID = -613462762639108431L;
+}
