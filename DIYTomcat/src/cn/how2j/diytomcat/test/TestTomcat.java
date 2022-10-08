29c18e5f0554708601781978fcf5356e54a6d3d2
APIInvoker <zx820@outlook.com>
2022-10-08 23:46:58 +0800

    DIYTomcat

---
+++ DIYTomcat/src/cn/how2j/diytomcat/test/TestTomcat.java
@@ -0,0 +1,49 @@
+package cn.how2j.diytomcat.test;
+
+import cn.how2j.diytomcat.Bootstrap;
+import cn.how2j.diytomcat.util.MiniBrowser;
+import cn.hutool.core.util.NetUtil;
+import cn.hutool.core.util.StrUtil;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Zheng Xin
+ * @since 2022/10/7 22:10
+ */
+public class TestTomcat {
+    private static int port = 18080;
+    private static String ip = "127.0.0.1";
+
+    @BeforeClass
+    public static void beforeClass() {
+        String[] args = new String[1];
+        new Thread(() -> Bootstrap.main(args)).start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // 所有测试开始前看diy tomcat 是否已经启动了
+        if (NetUtil.isUsableLocalPort(port)) {
+            System.err.println("请先启动 位于端口: " + port + " 的diy tomcat，否则无法进行单元测试");
+            System.exit(1);
+        }
+        System.out.println("检测到diy tomcat已经启动,开始进行单元测试");
+    }
+
+    @Test
+    public void testHelloTomcat() {
+        String html = getContentString("/");
+        Assert.assertEquals(html, "Hello DIY Tomcat from how2j.cn");
+        System.out.println("回归测试通过");
+    }
+
+    private String getContentString(String uri) {
+        String url = StrUtil.format("http://{}:{}{}", ip, port, uri);
+        return MiniBrowser.getContentString(url);
+    }
+}
--- springmvc/src/main/java/com/example/springmvc/TestController.java
+++ springmvc/src/main/java/com/example/springmvc/TestController.java
@@ -7,6 +7,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @Author zx
  * @Date 2022/9/25 3:30
@@ -22,5 +27,10 @@ public class TestController {
         model.addAttribute("vegetable", "卷心菜");
         // 跳转页面不能用@ResponseBody注解，因为@ResponseBody是将方法的返回值封装成方法的返回类型返回到页面上
         return "user/test";
+    }
+
+    @GetMapping("html.do")
+    public void html(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/static/test.html").forward(request,response);
     }
 }
